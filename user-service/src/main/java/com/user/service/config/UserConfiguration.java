29dbfe62d4f27b220338c9e0b5ab47b29a2e0551
Kiran Chauhan <kc7563330@gmail.com>
2025-09-24 21:30:12 +0530

    creating custom security table in db

--- user-service/src/main/java/com/user/service/config/UserConfiguration.java
+++ user-service/src/main/java/com/user/service/config/UserConfiguration.java
@@ -8,17 +8,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class UserConfiguration {
 
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        UserDetails john= User.builder().username("john").password("{noop}123").roles("USER").build();
+//        UserDetails mary= User.builder().username("mary").password("{noop}123").roles("USER","MANAGER","ADMIN").build();
+//        UserDetails suzi= User.builder().username("suzi").password("{noop}123").roles("USER","MANAGER","ADMIN").build();
+//        return new InMemoryUserDetailsManager(john,mary,suzi);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        UserDetails john= User.builder().username("Kiran").password("{noop}123").roles("USER").build();
-        UserDetails mary= User.builder().username("mary").password("{noop}456").roles("USER","MANAGER","ADMIN").build();
-        UserDetails suzi= User.builder().username("suzi").password("{noop}789").roles("USER","MANAGER","ADMIN").build();
-        return new InMemoryUserDetailsManager(john,mary,suzi);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -27,7 +39,8 @@ public class UserConfiguration {
                 configurer.requestMatchers(HttpMethod.GET, "/user").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/user/**").hasRole("USER")
                         .requestMatchers(HttpMethod.POST, "/user").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/user/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,  "/user/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH,"/user/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/user/**").hasRole("ADMIN")
         );
         httpSecurity.httpBasic(Customizer.withDefaults());
