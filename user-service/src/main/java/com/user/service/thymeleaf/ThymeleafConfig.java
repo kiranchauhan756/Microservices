4139d9e985dfd9e72fd56d54e60b56b07be71d21
Kiran Chauhan <kc7563330@gmail.com>
2025-09-25 17:05:33 +0530

    diff btwn get and post

--- user-service/src/main/java/com/user/service/thymeleaf/ThymeleafConfig.java
+++ user-service/src/main/java/com/user/service/thymeleaf/ThymeleafConfig.java
@@ -1,8 +1,10 @@
 package com.user.service.thymeleaf;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,9 +16,25 @@ public class ThymeleafConfig {
         return "hello_world";
     }
 
-    @GetMapping("/process-form")
-    public String processForm(@RequestParam("studentName") String studentName, Model model){
-        model.addAttribute("studentName",studentName);
+    @PostMapping("/process-form")
+    public String processForm(){
         return "process_form";
+    }
+
+//    @GetMapping("/process-form-v2")
+//    public String processFormV2(HttpServletRequest httpServletRequest,Model model){
+//        String name=httpServletRequest.getParameter("studentName");
+//        name=name.toUpperCase();
+//        String result="Yo "+name;
+//        model.addAttribute("message",result);
+//        return "process_form_v2";
+//    }
+
+    @PostMapping("/process-form-v2")
+    public String processFormV3(@RequestParam("studentName") String studentName,Model model){
+        studentName=studentName.toUpperCase();
+        String result="Yo I am from v3 "+studentName;
+        model.addAttribute("message",result);
+        return "process_form_v2";
     }
 }
